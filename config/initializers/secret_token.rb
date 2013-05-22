ee6ebbfff142ca1db01c5df593710076681ed4a4
Franciscog Gomez <francisco@Franciscogs-MacBook-Pro.local>
2013-05-22 13:52:43 -0700

    browser no se conecta a la aplicacion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PensamientoScaffold::Application.config.secret_token = 'f5360eff3f48c182dec0aa3fed2ab14d56530bd18c22528c57d564c96a0b5bb7ddbc4de00b03032934800c3b09edd9a74530e07ac9ef471ffcaf9518ea270cc7'
